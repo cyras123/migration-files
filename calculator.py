684a8c469661d78cdc416decb242a9cad21edb6e
cyras123 <sudiprana673@gmail.com>
2019-01-14 17:35:21 +0545

    first version of calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+first=int(input('enter 1st number:'))
+print(type(first))
+second=int(input("enter second number:"))
+operator=input("enter the operator:")
+print(type(operator))
+if operator == '+':
+    sum=first + second
+    print('the sum is',sum)
+elif operator=='-':
+    diff=first - second
+    print('the diff is',diff)
+elif operator=='*':
+    mul=first * second
+    print('the multiplication is',mul)
+
+else:
+    division=float(first / second)
+    print('the division is',division)
